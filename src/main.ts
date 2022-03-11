af8fcc832c3a8ee5ce371b9d4ace75ecc13e5ba4
Anton Reshetov <reshetov.art@gmail.com>
2022-03-11 10:45:59 +0300

    feat: add router & layout system

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
           customCollections: ['svg']
         })
       ],
-      dirs: ['src/components']
+      dirs: ['src/layouts', 'src/components']
     }),
     Icons({
       customCollections: {
